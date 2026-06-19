1662016751f77d8cccdbd214bb8db23532464145
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-19 20:28:42 -0300

    Aula 5

---
+++ Aula 5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula05;
+
+public class Program
+{
+    public static void Main()
+    {
+        double number1 = 10;
+        double number2 = 20;
+
+        double sum = number1+number2;
+        double sub = number1-number2;
+        double div = number1/number2;
+        double mult = number1*number2;
+        Console.WriteLine(sum + ", " +  sub + ", " + div + ", " + mult);
+        Console.WriteLine(div);
+    }
+}
