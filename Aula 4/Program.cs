f5887892bd26a8065868d4bc6cd8abc3c58314e0
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-19 20:13:00 -0300

    Aula 4

---
+++ Aula 4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-------- Login --------");
+
+        Console.WriteLine("Digite seu nome:  ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Digite seu nome:  ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("-----------------------");
+        
+        Console.WriteLine("Nome digitado: " + name);
+        Console.WriteLine("Idade digitada: " + age);
+    }
+}
