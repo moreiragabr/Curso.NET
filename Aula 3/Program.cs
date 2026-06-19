a7bd00a833e6bc74417948e7fcef9af8591f6dda
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-19 20:01:56 -0300

    Aula 3

---
+++ Aula 3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+        int number;
+        number = 10;
+        number = 20;
+        number = 30;
+
+        Console.WriteLine(number);
+
+        var x = 10;
+        var y = true;
+        var z = "string";
+
+        Console.WriteLine(x + ", " + y + ", " + z);
+
+        dynamic j;
+        j = 10;
+        j = "string";
+        j = true;
+
+        Console.WriteLine(j);
+    }
+}
