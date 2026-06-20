db373d99c0859961a805cabed66373ad0a0d3861
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-20 00:58:45 -0300

    Aula 6

---
+++ Aula 6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula06;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("============numero par ou impar============");
+        Console.WriteLine("coloca um numero: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int result = number % 2;
+
+        if (result == 0)
+        {
+            Console.WriteLine("numero par");
+        }
+        else
+        {
+            Console.WriteLine("numero impar");
+        }
+    }
+}
