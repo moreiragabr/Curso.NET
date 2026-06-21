37fd98945f270fa72f3472bda2b7c6a015ccb1e6
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-20 23:56:38 -0300

    Aula 15

---
+++ Aula 15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula15;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<int> numberList = new List<int>(){1,1,1,1,1,1,1,1,1,1};
+        int i = 0;
+
+        foreach(int number in numberList)
+        {
+            i+=number;
+            Console.WriteLine(i);
+        }
+    }
+}
