0b1181539c884e618f6cb67b79dca948e56663f8
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-20 22:37:05 -0300

    aula 11

---
+++ Aula 11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula11;
+
+public class Program()
+{
+    public static void Main()
+    {
+        bool bolean = true;
+
+        switch (bolean)
+        {
+            case true:
+            Console.WriteLine("\nTrue\n");
+            break;
+
+            case false:
+            Console.WriteLine("\nFalse\n");
+            break;
+        }
+    }
+}
