2db4eeea80c3247cdc7ce3833a7ed7c2a9a71b89
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-21 00:21:02 -0300

    aula 16

---
+++ Aula 16/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Aula16;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] letras = { "x", "y", "z", "j", "k" };
+
+        Console.WriteLine("\n" + letras[0] + letras[1] + letras[2] + "\n");
+
+        foreach (string letra in letras)
+        {
+            Console.WriteLine("\n" + letra + "\n");
+        }
+
+        Console.WriteLine("\n" + letras.Length + "\n");
+
+        int[] ints = new int[3];
+
+        foreach (int inteiro in ints)
+        {
+            Console.WriteLine("\n" + inteiro + "\n");
+        }
+
+        ints[0] = 1;
+        ints[1] = 2;
+        ints[2] = 3;
+
+        foreach (int inteiro in ints)
+        {
+            Console.WriteLine("\n" + inteiro + "\n");
+        }
+    }
+}
