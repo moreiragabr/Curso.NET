a77418007419ee6b03d6f7d551c50ea48beceaab
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-21 00:35:41 -0300

    Aula 17

---
+++ Aula 17/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Aula17;
+
+public class Program()
+{
+    public static void Main()
+    {
+        Console.WriteLine("\n");
+
+        List<char> letras = new List<char>
+        {
+          'A',
+          'B',
+          'C',
+          'D',
+        };
+
+        foreach (char letra in letras)
+        {
+            Console.WriteLine(letra);
+        }
+
+        Console.WriteLine("\n");
+
+        letras.AddRange('E', 'F');
+        letras.Add('G');
+
+        foreach (char letra in letras)
+        {
+            Console.WriteLine(letra);
+        }
+
+        Console.WriteLine("\n");
+
+        letras.Remove('G');
+
+        foreach (char letra in letras)
+        {
+            Console.WriteLine(letra);
+        }
+
+        Console.WriteLine("\n");
+    }
+}
