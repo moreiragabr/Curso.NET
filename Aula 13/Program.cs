1a26571b91c9d1227feaf71ddf824b318b85e6bf
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-20 23:00:39 -0300

    aula 13

---
+++ Aula 13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula13;
+
+public class Program()
+{
+    public static void Main()
+    {
+        int i = 0;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+    }
+}
