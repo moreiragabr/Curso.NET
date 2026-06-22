c3f2d070d90cc49d77eb11ee0698622651f704af
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-22 02:00:23 -0300

    Aula 27

---
+++ Aula 27/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula27;
+
+public class Cake
+{
+    public string Flavor;
+    public string Coverage;
+    public Cake(string flavor, string coverage)
+    {
+        Coverage = coverage;
+        Flavor = flavor;
+    }
+}
---
+++ Aula 27/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Aula27;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cake = new Cake("chocolate", "chocolate");
+    }
+}
