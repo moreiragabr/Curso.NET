38b009d6e0f22a8741db747fc9657e989ab8d3b0
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-22 01:46:43 -0300

    Aula 26

---
+++ Aula 26/Cake.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula26;
+
+public class Cake
+{
+    public string Flavor;
+    public string Coverage;
+    public Cake()
+    {
+        Flavor = "chocolate";
+        Coverage = "chocolate";
+    }
+}
---
+++ Aula 26/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula26;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cake = new Cake(){};
+
+        Console.WriteLine(cake.Coverage);
+        Console.WriteLine(cake.Flavor);
+    }
+}
