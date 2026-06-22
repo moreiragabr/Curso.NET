bf1544c3c3d47837702b3a8a43a429bf9e689ea7
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-21 21:58:19 -0300

    Aula 24

---
+++ Aula 24/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula24;
+
+public class Person
+{
+    public long Id;
+    public string Name;
+    public int Age;
+
+    public bool ifLegalPerson(int age)
+    {
+        if (age >= 18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Aula 24/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aula24;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person person = new Person()
+        {
+            Age = 17
+        };
+
+        Person person2 = new Person()
+        {
+            Age = 18
+        };
+
+        if (person.ifLegalPerson(person.Age))
+        {
+            Console.WriteLine("Legal.");
+        }
+        else
+        {
+            Console.WriteLine("Ilegal.");
+        }
+
+        if (person2.ifLegalPerson(person2.Age))
+        {
+            Console.WriteLine("Legal.");
+        }
+        else
+        {
+            Console.WriteLine("Ilegal.");
+        }
+    }
+}
