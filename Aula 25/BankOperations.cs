827259d89fd1849e8e30d03b0b2f69fc4a301c50
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-22 01:28:10 -0300

    Aula 25

---
+++ Aula 25/BankOperations.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula25;
+
+public class BankOperations
+{
+    decimal balance = 1000;
+
+    public string CheckBalance()
+    {
+        return $"Seu saldo é de R${balance}";
+    }
+}
---
+++ Aula 25/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Aula25;
+
+public class Program
+{
+    public static void Main()
+    {
+        BankOperations bankOperations = new BankOperations(){};
+
+        Console.WriteLine(bankOperations.CheckBalance());
+    }
+}
