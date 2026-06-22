4893018a289cafb1df01c2f45ec6ef761c1baa52
moreiragabr <gabrielmoreiracarminatti@gmail.com>
2026-06-21 21:40:45 -0300

    Aula 23

---
+++ Aula 23/Bank.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula23;
+
+public class Bank
+{
+    public long Id;
+    public string Name;
+
+    public void RequestNewCard()
+    {
+        Console.WriteLine("\nmétodo teste...\n");
+    }
+}
---
+++ Aula 23/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula23;
+
+public class Program
+{
+    public static void Main()
+    {
+        Bank bank = new Bank(){};
+        bank.RequestNewCard();
+    }
+}
